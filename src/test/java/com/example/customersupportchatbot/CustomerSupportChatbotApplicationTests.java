a329a5bb0eac616bae8f1b121b44b19704a2fc5c
Muhammad Yahya <yahya@wightcode.co.uk>
2025-05-11 20:08:32 +0100

    refactored

---
+++ src/test/java/com/example/customersupportchatbot/CustomerSupportChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customersupportchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSupportChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
