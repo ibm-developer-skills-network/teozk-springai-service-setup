f3e4f09a47fec18267ff522902d159351685c7bf
Muhammad Yahya <yahya@wightcode.co.uk>
2025-05-11 19:46:53 +0100

    first commit

---
+++ CustomerSupportChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.customersupportchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSupportChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSupportChatbotApplication.class, args);
+	}
+
+}
